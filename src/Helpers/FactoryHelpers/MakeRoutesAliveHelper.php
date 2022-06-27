57e0ec0f3cdbe4d28e1b49a2962ad0f56f4109d8
Hani Abdul-Fattah <haniabdulfattah32@gmail.com>
2022-06-27 08:46:25 +0300

    Separating routes into api and web files according to the mode

--- src/Helpers/FactoryHelpers/MakeRoutesAliveHelper.php
+++ src/Helpers/FactoryHelpers/MakeRoutesAliveHelper.php
@@ -4,8 +4,21 @@ namespace Hani221b\Grace\Helpers\FactoryHelpers;
 
 use Hani221b\Grace\Helpers\MakeStubsAliveHelper;
 
-class MakeRoutesAliveHelper 
+class MakeRoutesAliveHelper
 {
+    /**
+     * returns the path of the requested route file
+     * @return string
+     */
+    public static function getRouteFileName()
+    {
+        if (config('grace.mode') === 'api') {
+            $filename = base_path() . '\routes\api.php';
+        } else if (config('grace.mode') === 'blade') {
+            $filename = base_path() . '\routes\web.php';
+        }
+        return $filename;
+    }
 
     /**
      * append use controller statement at the to of the route file
@@ -13,14 +26,15 @@ class MakeRoutesAliveHelper
      * @param @controller_name
      * @return void
      */
-    public static function appendUseController($stubVariables = [], $controller_name){
+    public static function appendUseController($stubVariables = [], $controller_name)
+    {
         $controller_namespace = $stubVariables['controller_namespace'];
         $use_controller = "use $controller_namespace\\$controller_name;";
-        $filename = base_path() . '\routes\web.php';
+        $filename = self::getRouteFileName();
         $line_i_am_looking_for = 1;
-        $lines = file( $filename , FILE_IGNORE_NEW_LINES );
-        $lines[$line_i_am_looking_for] = "\n".$use_controller;
-        file_put_contents( $filename , implode( "\n", $lines ) );
+        $lines = file($filename, FILE_IGNORE_NEW_LINES);
+        $lines[$line_i_am_looking_for] = "\n" . $use_controller;
+        file_put_contents($filename, implode("\n", $lines));
     }
     /**
      * append resource routes for a certain table
@@ -30,15 +44,15 @@ class MakeRoutesAliveHelper
 
     public static function appendRoutes($stubVariables = [])
     {
-        $routes_file = base_path() . '\routes\web.php';
-        $opened_file  = fopen($routes_file, 'a');
+        $routes_file = self::getRouteFileName();
+        $opened_file = fopen($routes_file, 'a');
         $controller_name = MakeStubsAliveHelper::getSingularClassName($stubVariables['table_name']) . "Controller";
         $table_name = $stubVariables['table_name'];
         $routes_template = "
 // $table_name
 Route::resource('$table_name', $controller_name::class);
         ";
- 
+
         self::appendUseController($stubVariables, $controller_name);
         fwrite($opened_file, $routes_template);
         fclose($opened_file);
