af6637f6ccee95691cabcadff855df664746a3c6
Hani Abdulfattah <haniabdulfattah32@gmail.com>
2022-07-30 15:38:03 -0500

    adding validation rules

--- src/Views/includes/add_validation.blade.php
+++ src/Views/includes/add_validation.blade.php
@@ -44,17 +44,17 @@
                                                 <v-row v-for="(rule, index) in rules" :key="rule.id">
                                                     <v-row v-if="rule.field === {{json_encode($field)}}">
                                                         <input type="hidden" name="validation[{{$index}}][field]" value="{{$field}}">
-                                                        <v-col cols="12" md="6">
+                                                        <v-col cols="6">
                                                             <v-autocomplete :items="rulesList" outlined name="validation[{{$index}}][rules][]">
                                                             </v-autocomplete>
                                                         </v-col>
-                                                        <v-col cols="12" md="3">
+                                                        <v-col cols="3">
                                                             <v-btn color="error" v-on:click="deleteRule(index)">Remove Rule</v-btn>
                                                         </v-col>
                                                     </v-row>
                                                 </v-row>
                                                 <v-row>
-                                                    <v-col cols="12" md="3">
+                                                    <v-col cols="3">
                                                         <v-btn color="success" v-on:click="addRule({{json_encode($field)}})">Add Rule</v-btn>
                                                     </v-col>
                                                 </v-row>
@@ -82,15 +82,21 @@
         el: "#validation",
         data: () => ({
             rulesList: [
-                'rule1',
-                'rule2',
-                'rule3',
+                'accepted',
+                'accepted_if',
+                'active_url','after','after_or_equal','alpha', 'alpha_dash','alpha_num','array','bail','before','before_or_equal',
+                'between','boolean','confirmed','current_password','date','date_equals','date_format','declined','declined_if',
+                'different','digits','digits_between','dimensions','distinct','email','ends_with','enum','exclude','exclude_if',
+                'exclude_unless','exclude_with','exclude_without','exists','file','filled','gt','gte','image','in','in_array',
+                'integer','ip','ipv4','ipv6','json','lt','lte','mac_address','max','mimetypes','mimes','min','multiple_of', 'not_in',
+                'not_regex','nullable','numeric','password','present','prohibited','prohibited_if','prohibited_unless','prohibits',
+                'regex','required','required_if','required_unless','required_with','required_with_all','required_without','required_without_all',
+                'required_array_keys','same','size','starts_with','string','timezone','unique','url','uuid',
             ],
             rules: [],
         }),
         methods: {
             addRule(field) {
-                console.log('rules',field);
                 this.rules.push({
                     field: field,
                     time: Date.now()
