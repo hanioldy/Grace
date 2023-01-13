5d5141dc14a459e2863620b8321550c40d00cc51
Hani Abdulfattah <haniabdulfattah32@gmail.com>
2023-01-13 12:56:20 -0500

    improved deleteDir function

--- src/Support/File.php
+++ src/Support/File.php
@@ -86,21 +86,21 @@ class File
 
     public static function deleteDir($dirPath): void
     {
-        if (!is_dir($dirPath)) {
-            throw new InvalidArgumentException("$dirPath must be a directory");
-        }
-        if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+        if (is_dir($dirPath)) {
+                    if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
             $dirPath .= '/';
-        }
-        $files = glob($dirPath . '*', GLOB_MARK);
-        foreach ($files as $file) {
-            if (is_dir($file)) {
-                self::deleteDir($file);
-            } else {
-                unlink($file);
             }
+            $files = glob($dirPath . '*', GLOB_MARK);
+            foreach ($files as $file) {
+                if (is_dir($file)) {
+                    self::deleteDir($file);
+                } else {
+                    unlink($file);
+                }
+            }
+          rmdir($dirPath);
         }
-        rmdir($dirPath);
+
     }
 
         /**
